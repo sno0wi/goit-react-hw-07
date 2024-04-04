e55374a306a36b9bf5f510bb519c9bca5e2b68e2
sno0wi <sno0wi20@gmail.com>
2024-04-04 13:52:46 +0300

    add loader and errorMessage

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,16 +2,23 @@ import Contact from '../Contact/Contact.jsx';
 import { selectContacts } from '../../redux/contactsReducer.js';
 import { selectNameFilter } from '../../redux/filtersReducer.js';
 import { useSelector } from 'react-redux';
+import { selectError, selectLoading } from '../../redux/contactsReducer.js';
+import Loader from '../Loader/Loader.jsx';
+import ErrorMessage from '../ErrorMessage/ErrorMessage.jsx';
 
 const ContactList = () => {
   const contacts = useSelector(selectContacts);
   const nameContactFilter = useSelector(selectNameFilter);
+  const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
   const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(nameContactFilter.toLowerCase())
   );
 
   return (
     <>
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
       {filteredContacts.map(contact => (
         <Contact key={contact.id} contact={contact} />
       ))}
--- src/redux/contactsReducer.js
+++ src/redux/contactsReducer.js
@@ -62,5 +62,7 @@ const contactsSlice = createSlice({
 });
 
 export const selectContacts = state => state.contacts.contacts.items;
+export const selectLoading = state => state.contacts.contacts.loading;
+export const selectError = state => state.contacts.contacts.error;
 
 export const contactsReducer = contactsSlice.reducer;
