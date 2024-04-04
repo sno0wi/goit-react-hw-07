7df6a08561f91ec4c10141171014b9aec51adc8b
sno0wi <sno0wi20@gmail.com>
2024-04-04 11:02:54 +0300

    done api

--- src/services/api.js
+++ src/services/api.js
@@ -6,3 +6,18 @@ export const fetchContacts = async () => {
   );
   return data;
 };
+
+export const addContact = async contactData => {
+  const response = await axios.post(
+    'https://660e52936ddfa2943b3663ff.mockapi.io/contacts',
+    contactData
+  );
+  return response.data;
+};
+
+export const deleteContact = async contactId => {
+  const response = await axios.delete(
+    `https://660e52936ddfa2943b3663ff.mockapi.io/contacts/${contactId}`
+  );
+  return response.data;
+};
