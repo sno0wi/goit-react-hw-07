7bfbe5c8dd47bae62e8021c086949e6aad7c1ea3
sno0wi <sno0wi20@gmail.com>
2024-04-04 13:35:37 +0300

    fixed filter

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,18 @@
 import Contact from '../Contact/Contact.jsx';
 import { selectContacts } from '../../redux/contactsReducer.js';
-// import { selectNameFilter } from '../../redux/filtersReducer.js';
+import { selectNameFilter } from '../../redux/filtersReducer.js';
 import { useSelector } from 'react-redux';
 
 const ContactList = () => {
   const contacts = useSelector(selectContacts);
-  // const nameContactFilter = useSelector(selectNameFilter);
-  // const filteredContacts = contacts.filter(contact =>
-  //   contact.text.name.toLowerCase().includes(nameContactFilter.toLowerCase())
-  // );
+  const nameContactFilter = useSelector(selectNameFilter);
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(nameContactFilter.toLowerCase())
+  );
 
   return (
     <>
-      {contacts.map(contact => (
+      {filteredContacts.map(contact => (
         <Contact key={contact.id} contact={contact} />
       ))}
     </>
